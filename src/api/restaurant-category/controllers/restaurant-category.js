880a12a2e41cb722c109035ac9bd07ad77809ae2
brendansiraky <brendansirakydeveloper@gmail.com>
2022-08-08 14:26:51 -0500

    added APIs for frontend interaction.

---
+++ src/api/restaurant-category/controllers/restaurant-category.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  restaurant-category controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::restaurant-category.restaurant-category');
---
+++ src/api/restaurant-category/services/restaurant-category.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * restaurant-category service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::restaurant-category.restaurant-category');
